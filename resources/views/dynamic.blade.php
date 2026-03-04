a65010a81e3c6f9a1a9a029fbdbefa8bdb87c364
Parth Chauhan <parthchauhan25125@gmail.com>
2026-03-04 11:44:21 +0530

    TASK4 Get Value Through URL and display in View Completed

---
+++ resources/views/dynamic.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Dynamic Task List</title>
+</head>
+<body>
+    <h1>My Dynamic Task List</h1>
+    <p>Value Passed by user: <strong>{{ $value }}</strong></p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,21 @@ Route::get("/task/{name}",function($name){
      return 'Task name is: '. $name;
 });
 
+//TASK 2
+
 Route::get("/task/{name}/{priority}",function($name,$priority){
     return 'Task:'.$name.' Priority:'.$priority;
 });
 
+//TASK 3
+
 Route::get("/tasklist",function(){
     return view('taskList');
+});
+
+//TASK 4
+Route::get('/dynamic/{value}',function($value){
+    return view('dynamic')->with('value',$value);
 });
 
 Route::get('/', function () {
